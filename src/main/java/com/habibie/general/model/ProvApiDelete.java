b6282242c9a6e3d90a73f35e2fe7d77b603ca41c
rizaldihabibie <rialdihabibie28>
2021-12-03 14:33:42 +0700

    initial commit

---
+++ src/main/java/com/habibie/general/model/ProvApiDelete.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.habibie.general.model;
+
+/**
+ *
+ * @author root
+ */
+public class ProvApiDelete {
+    private String contentid;
+    private int deleteupoint;
+
+    public String getContentid() {
+        return contentid;
+    }
+
+    public void setContentid(String contentid) {
+        this.contentid = contentid;
+    }
+
+    public int getDeleteupoint() {
+        return deleteupoint;
+    }
+
+    public void setDeleteupoint(int deleteupoint) {
+        this.deleteupoint = deleteupoint;
+    }
+    
+    
+}
